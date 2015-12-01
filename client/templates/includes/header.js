65c5e51e62115fce14663276874ff444eee298cd
benjimouse <ben@benbat.com>
2015-12-01 23:53:10 +0000

    Sorting out not displaying
    Steps weren’t displaying, that has now been resolved, also starting to
    work on amending the serving time.

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -7,6 +7,13 @@ Template.header.helpers({
         'use strict';
         return Session.get('totalCookingTime');
     },
+    servingTime: function () {
+        'use strict';
+        var meal = Meals.findOne();
+        if (meal) {
+            return new DateFormat().showDateTimeForEditing(meal.serveTime);
+        }
+    },
     serveTime: function () {
         'use strict';
         var meal = Meals.findOne();
@@ -18,7 +25,8 @@ Template.header.helpers({
         'use strict';
         var currTime = Session.get('currentTime'),
             startTime = Session.get('startTime'),
-            secondsToGo, timeToGo;
+            secondsToGo,
+            timeToGo;
         if (startTime) {
             secondsToGo = Math.floor((startTime.getTime() - currTime) / 1000);
             timeToGo = new Time().createTimeFromSeconds(secondsToGo).timeForDisplay();
@@ -30,5 +38,18 @@ Template.header.helpers({
         if (Session.get('startTime')) {
             return new DateFormat().showFormattedDate(Session.get('startTime'));
         }
+    },
+    editingServingTime: function () {
+        'use strict';
+        return !!Session.get('editingServingTime');
+    }
+});
+Template.header.events({
+    'click #servingTime': function (e) {
+        'use strict';
+        e.preventDefault();
+        console.log(this);
+        Session.set('editingServingTime', true);
+        //TODO: Finish
     }
 });
--- client/templates/steps/steps_list.js
+++ client/templates/steps/steps_list.js
@@ -1,13 +1,21 @@
+/*global Template */
+/*global Session */
+/*global Steps */
+/*jslint nomen: true */
+
 Template.stepsList.helpers({
     steps: function () {
         'use strict';
-        var mealId = Session.get('currentMeal')._id;
-        return Steps.find({
-            mealId: mealId
-        }, {
-            sort: {
-                totalDuration: -1
-            }
-        });
+        var mealId = Session.get('currentMeal')._id,
+            allSteps = Steps.find({
+                mealId: mealId
+            }, {
+                sort: {
+                    totalDuration: -1
+                }
+            });
+        return allSteps;
     }
 });
+
+/*jslint nomen: false */
